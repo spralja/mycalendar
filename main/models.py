ebb204d76ffa68e3d16abb00252db5474467baa4
Robert Spralja <56681655+spralja@users.noreply.github.com>
2021-10-05 17:37:55 +0200

    Created days of the week model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class DayOfTheWeek(models.Model):
+    class Name(models.TextChoices):
+        MONDAY = 'Monday', _('Monday')
+        TUESDAY = 'Tuesday', _('Tuesday')
+        WEDNESDAY = 'Wednesday', _('Wednesday')
+        THURSDAY = 'Thursday', _('Thursday')
+        FRIDAY = 'Friday', _('Friday')
+        SATURDAY = 'Saturday', _('Saturday')
+        SUNDAY = 'Sunday', _('Sunday')
+
+    name = models.CharField(choices=Name.choices, max_length=9)
+
+    def __str__(self):
+        return self.name
+
