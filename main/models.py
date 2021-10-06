65f881a5c4da9ac338d9f2ee3a44177767d95ba2
Robert Spralja <56681655+spralja@users.noreply.github.com>
2021-10-06 20:45:23 +0200

    Made switch static for optimisation

--- main/models.py
+++ main/models.py
@@ -2,6 +2,15 @@ from django.db import models
 
 
 class Event(models.Model):
+    switch = {
+        'Monday': 1,
+        'Tuesday': 2,
+        'Wednesday': 3,
+        'Thursday': 4,
+        'Friday': 5,
+        'Saturday': 6,
+        'Sunday': 7,
+    }
     title = models.CharField(max_length=25)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
@@ -13,14 +22,4 @@ class Event(models.Model):
 
     def get_day(self):
         string = self.start_time.strftime("%A")
-        switch = {
-           'Monday': 1,
-           'Tuesday': 2,
-           'Wednesday': 3,
-           'Thursday': 4,
-           'Friday': 5,
-           'Saturday': 6,
-           'Sunday': 7,
-        }
-
-        return switch[string]
+        return self.switch[string]
