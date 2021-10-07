782337b9a553da58420c227a0df0c61b235975e7
Robert Spralja <56681655+spralja@users.noreply.github.com>
2021-10-07 02:45:04 +0200

    update calndr but scrapped for now

--- main/calendar.py
+++ main/calendar.py
@@ -1,30 +1,26 @@
 class Cell:
     width = 150
 
-    def __init__(self, pos_y, height, background_color="#FFFFFF"):
+    def __init__(self, pos_y, height, background_color="#000000"):
         self.pos_y = pos_y
         self.height = height
         self.background_color = background_color
 
+
 class Calendar:
     def __init__(self):
         self.table = [
-            [Cell(0, 480), Cell(480, 0)],
-            [Cell(0, 480), Cell(480, 0)],
-            [Cell(0, 480), Cell(480, 0)],
-            [Cell(0, 480), Cell(480, 0)],
-            [Cell(0, 480), Cell(480, 0)],
-            [Cell(0, 480), Cell(480, 0)],
-            [Cell(0, 480), Cell(480, 0)],
+            [Cell(0, 480), Cell(0, 480), Cell(0, 480), Cell(0, 480), Cell(0, 480), Cell(0, 480), Cell(0, 480), ],
+            [Cell(480, 0), Cell(480, 0), Cell(480, 0), Cell(480, 0), Cell(480, 0), Cell(480, 0), Cell(480, 0), ],
         ]
 
     def add_event(self, event):
         x = event.start_time.weekday()
         pos_y = event.start_time.hour*20 + int(event.start_time.minute / 3)
         duration = event.end_time - event.start_time
-        height = duration.hour*20 + int(duration.minute / 3)
+        height = int(duration.total_seconds()/180)
         event_cell = Cell(pos_y, height, event.color)
-        for y in range(len(self.table[x])):
+        for y in range(len(self.table[:][x])):
             current_cell = self.table[x][y]
             if current_cell.pos_y > pos_y:
                 former_cell = self.table[x][y - 1]
